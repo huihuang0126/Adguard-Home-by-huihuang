396c689c5fdacb05de525e6ef67283b5d5dac3d5
huixiaohuang <38674658+huihuang0126@users.noreply.github.com>
2026-01-22 02:43:46 +0800

    更新 tool.sh

--- scripts/tool.sh
+++ scripts/tool.sh
@@ -30,8 +30,8 @@ toggle_adguardhome() {
 }
 
 case "$1" in
-    start)  start_adguardhome ;;
-    stop)   stop_adguardhome ;;
-    toggle) toggle_adguardhome ;;
-    *)      echo "使用方法: $0 {start|stop|toggle}"; exit 1 ;;
+start)start_adguardhome;;
+stop)stop_adguardhome;;
+toggle)toggle_adguardhome;;
+*)echo "使用方法: $0 {start|stop|toggle}";exit 1;;
 esac
